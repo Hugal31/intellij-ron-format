5d14b570fbf68956a269b7c4137dabddafe35449
Hugo Laloge <hugo.laloge@gmail.com>
2019-04-24 08:39:52 +0200

    feat: add simple file icon

--- src/com/ronformat/RonFileType.java
+++ src/com/ronformat/RonFileType.java
@@ -35,6 +35,6 @@ public class RonFileType extends LanguageFileType {
     @Nullable
     @Override
     public Icon getIcon() {
-        return null;
+        return RonIcons.FILE;
     }
 }
---
+++ src/com/ronformat/RonIcons.java
@@ -0,0 +1,11 @@
+package com.ronformat;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class RonIcons {
+
+    public static final Icon FILE = IconLoader.getIcon("/com/ronformat/icons/ron-file.svg");
+
+}
