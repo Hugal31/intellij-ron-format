9b434a847894a7aea3d83fd9dcb47d1e0506516d
Hugo Laloge <hugo.laloge@gmail.com>
2019-05-02 10:31:06 +0200

    chore: use Gradle and allow tests

--- src/test/java/com/ronformat/tests/RonParsingTest.java
+++ src/test/java/com/ronformat/tests/RonParsingTest.java
@@ -6,7 +6,12 @@ import com.ronformat.RonParserDefinition;
 public class RonParsingTest extends ParsingTestCase {
 
     public RonParsingTest() {
-        super("testData", "ron", new RonParserDefinition());
+        super("parsingTests", "ron", new RonParserDefinition());
+    }
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/resources";
     }
 
     public void testParsingTestData() {
@@ -15,11 +20,11 @@ public class RonParsingTest extends ParsingTestCase {
 
     @Override
     protected boolean skipSpaces() {
-        return false;
+        return true;
     }
 
     @Override
     protected boolean includeRanges() {
-        return true;
+        return false;
     }
 }
